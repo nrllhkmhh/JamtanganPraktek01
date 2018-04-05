aa98b74d23c8861e07448de546ba8380e023764e
Nurul Hikmah <34591217+nrllhkmhh@users.noreply.github.com>
2018-04-06 01:55:11 +0800

    Membuat Atribut dan Behavior

--- src/jamtanganpraktek01/JamTangan.java
+++ src/jamtanganpraktek01/JamTangan.java
@@ -10,5 +10,17 @@ package jamtanganpraktek01;
  * @author Nurul
  */
 public class JamTangan {
+    String Warna;
+    String Merk;
+    String JenisJam;
+    String JenisTaliJam;
+    String KekuatanJam;
     
+    void cetakInfo(){
+     System.out.println("Warna          ="+Warna);
+     System.out.println("Merk           ="+Merk);
+     System.out.println("Jenis Jam      ="+JenisJam);
+     System.out.println("Jenis Tali Jam ="+JenisTaliJam);
+     System.out.println("Kekuatan Jam   ="+KekuatanJam);
+    }
 }
